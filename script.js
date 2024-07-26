efeaa4a63d06d9bd1c8bca070d0384314466c590
thiagogbarbosa <tthiagogb@gmail.com>
2024-07-26 12:14:49 +0100

    Showing a bug

--- script.js
+++ script.js
@@ -21,3 +21,6 @@ function getRandomIndex() {
 };
 
 const body = document.querySelector("body");
+const bgHexCodeSpanElement = document.querySelector("bg-hex-code");
+
+console.log(bgHexCodeSpanElement);
