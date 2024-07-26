f3bb9dec073f1593d9163bda0db6f81916040772
thiagogbarbosa <tthiagogb@gmail.com>
2024-07-26 12:12:46 +0100

    getRandomIndex function

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ const darkColorsArr = [
     "#2C3E50",
     "#800020",
   ];
+
+function getRandomIndex() {
+
+    //Math.floor rounds a number down to the nearest whole number
+    const randomIndex = Math.floor(darkColorsArr.length * Math.random());
+    return randomIndex;
+};
+
+const body = document.querySelector("body");
