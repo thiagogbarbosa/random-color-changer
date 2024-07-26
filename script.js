82886727f658d8fe8363a1c8cb1910a764918270
thiagogbarbosa <tthiagogb@gmail.com>
2024-07-26 12:01:25 +0100

    Initializing html and css

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //Learn Basic Debugging Building a Random Background Color Changer - freecodecamp.org
+
+const darkColorsArr = [
+    "#2C3E50",
+    "#34495E",
+    "#2C2C2C",
+    "#616A6B",
+    "#4A235A",
+    "#2F4F4F",
+    "#0E4B5A",
+    "#36454F",
+    "#2C3E50",
+    "#800020",
+  ];
